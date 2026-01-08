40262028edf6be2152a172ef74b9dd96ec2344d7
Gaurav G Sonavane <gauravsonavane02@gmail.com>
2026-01-08 20:26:05 +0530

    Add files via upload

---
+++ Basic_operation.py
@@ -0,0 +1,16 @@
+# Addition
+
+a = int(input("Enter The First The Number: "))
+b = int(input("Enter The Second The Number: "))
+
+c =a+b
+# Subtraction
+d = a-b
+# Multiplication
+e = a*b
+# Division
+f = a/b
+print("Addition is:",c)
+print("Subtraction is:",d)
+print("Multiplication is:",e)
+print("Division is:",f)
